fcd123f0ee53c926e3c7100010cef4a7a8a35e51
Doha <harbiid64@gmail.com>
2025-08-11 04:34:09 +0300

    Initial commit

---
+++ Bank/Models/CurrentAccount.cs
@@ -0,0 +1,30 @@
+﻿using Bank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Models
+{
+    internal class CurrentAccount : BankAccount
+    {
+        public decimal OverdraftLimit { get; set; }
+
+        public CurrentAccount(string fullName, string nationalId, string phoneNumber, string address, double balance, decimal overdraftLimit)
+            : base(fullName, nationalId, phoneNumber, address, balance)
+        {
+            OverdraftLimit = overdraftLimit;
+        }
+
+        public override decimal CalculateInterest() => 0;
+
+        public override void ShowAccountDetails()
+        {
+            base.ShowAccountDetails();
+            Console.WriteLine($"Overdraft limit : {OverdraftLimit:C}");
+        }
+
+    }
+}
