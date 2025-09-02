dc4af3df0d01167ea88cc809730f1c1ec56c1310
Doha <harbiid64@gmail.com>
2025-09-02 15:14:50 +0300

    Updated file content

--- calc_extension_method/obj/Debug/net9.0/calc_extension_method.AssemblyInfo.cs
+++ calc_extension_method/obj/Debug/net9.0/calc_extension_method.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calc_extension_method")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a131352230389840c4840490f409d45472ce8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("calc_extension_method")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calc_extension_method")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
